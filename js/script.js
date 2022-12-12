284d14584e47594fcde7ce6ed104374a05bf3f00
Benas <benorno@Benass-MacBook-Pro.local>
2022-12-12 15:32:55 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let a = 1;
+console.log('Kintamojo a reiksme: ' + a);
+a = 2;
+console.log('Kintamojo a reiksme: ' + a);
+
+
+let b = 'Benas Asmontas';
+let c = 'Mano vardas ';
+let d = c + b;
+console.log(d);
+
+let metai = 2002;
+let menuo = 5;
+let diena = 3;
+let ats = metai - 2000 + menuo + diena;
+console.log(ats);
