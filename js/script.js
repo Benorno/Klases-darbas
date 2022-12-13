572b09d70aa71381430472d28ff1d0bbcc0027c7
Benorno <djbenorno@gmail.com>
2022-12-13 14:07:35 +0200

    js uzduotys

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-let a = 1;
-console.log('Kintamojo a reiksme: ' + a);
-a = 2;
-console.log('Kintamojo a reiksme: ' + a);
+//1  uzduotis
+let r = 15;
+let pi = 3.14;
+let c = 2 * pi * r;
+window.alert("apskritimo ilgis: " + c);
+let b = r ** 2;
+let a = pi * b;
+window.alert("apskritimo plotas: " + a);
 
-
-let b = 'Benas Asmontas';
-let c = 'Mano vardas ';
-let d = c + b;
-console.log(d);
-
-let metai = 2002;
-let menuo = 5;
-let diena = 3;
-let ats = metai - 2000 + menuo + diena;
-console.log(ats);
+//2    uzduotis
+//no idea
+let a1 = -5;
+let b1 = 0;
+let c1 = -4;
+let d1 = 4;
+let min = Math.min(a1, b1, c1, d1);
+let max = Math.max(a1, b1, c1, d1);
+document.write(`a = ${a1}, b = ${b1}, c = ${c1}, d = ${d1} <br>`);
+document.write(`x = ${min}, y = ${max}`);
+//no idea
